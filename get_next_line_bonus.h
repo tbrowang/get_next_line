dcb54ab758eadd346e2c50c1682e42f22d0f6e0a
Tom BROWANG--LEVITRE <tbrowang@e2r7p6.clusters.42paris.fr>
2021-12-15 22:58:38 +0100

    change name of bonus header file

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbrowang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 05:29:30 by tbrowang          #+#    #+#             */
-/*   Updated: 2021/12/10 04:44:00 by tbrowang         ###   ########.fr       */
+/*   Updated: 2021/12/15 22:57:54 by tbrowang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <stddef.h>
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4096
